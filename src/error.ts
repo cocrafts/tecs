0d36c4bb5a8add89f020c424f8eeedeeafea46f8
Tan Le <104194494+tanlethanh@users.noreply.github.com>
2025-06-07 15:05:49 +0700

    refactor: update error handling to return TECSError instances instead of throwing

--- src/error.ts
+++ src/error.ts
@@ -9,38 +9,38 @@ export default class TECSError extends Error {
   }
 
   static stateRunnerIsRegistered(state: unknown) {
-    throw new TECSError(`[StateMachine] State '${state}' is already registered with a runner`);
+    return new TECSError(`[StateMachine] State '${state}' is already registered with a runner`);
   }
 
   static initialStateNotExist(state:unknown) {
-    throw new TECSError(`[StateMachine] Initial '${state}' is not available in state map`);
+    return new TECSError(`[StateMachine] Initial '${state}' is not available in state map`);
   }
 
   static accessImmutableData() {
-    throw new TECSError('Data is immutable in this context, it is only mutable in commands');
+    return new TECSError('Data is immutable in this context, it is mutable only in commands');
   }
 
   static notInitialToStart() {
-    throw new TECSError('Can not call \'start\' when the current state is not \'initial\'');
+    return new TECSError('Can not call \'start\' when the current state is not \'initial\'');
   }
 
   static notWaitForActionToPushAction() {
-    throw new TECSError('Can not push new action when the current state is not \'wait_action\'');
+    return new TECSError('Can not push new action when the current state is not \'wait_action\'');
   }
 
   static notAllowAsyncCommand() {
-    throw new TECSError('Async commands is not allowed, use synchronous commands to maintain mutability');
+    return new TECSError('Async commands is not allowed, use synchronous commands to maintain mutability');
   }
 
   static requireInState(state: unknown, thing: unknown) {
-    throw new TECSError(`Require ${thing} to handle '${state}'`);
+    return new TECSError(`Require ${thing} to handle '${state}'`);
   }
 
   static requireHandlerForAction(action: unknown) {
-    throw new TECSError(`Require handler to handle '${action}'`);
+    return new TECSError(`Require handler to handle '${action}'`);
   }
 
   static requireCommandExecutor(command: unknown) {
-    throw new TECSError(`Require command executor for '${command}'`);
+    return new TECSError(`Require command executor for '${command}'`);
   }
 }
